bfa9bd9a9260941208070a0d1bda771a75f4ea1e
Ronny Bräunlich <ronny.braeunlich@cbc.de>
2024-06-11 13:05:42 +0200

    Fill predict.py and train.py

---
+++ predict.py
@@ -0,0 +1,99 @@
+import argparse
+import torch
+from torchvision import models
+import numpy as np
+from PIL import Image
+
+
+def get_input_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_image', type=str, help='path to the image file')
+    parser.add_argument('checkpoint', type=str,
+                        help='path to where the model checkpoint has been saved')
+    parser.add_argument('--top_k', type=int, default=5, help='Top K classes')
+    parser.add_argument('--category_names ', type=str, help='mapping to category names')
+    parser.add_argument('--gpu', default=True, action='store_true',
+                        help='Use GPU?')
+    return parser.parse_args()
+
+def load_model(checkpoint):
+    model_checkpoint = torch.load(checkpoint)
+    model = models.get_model(model_checkpoint['arch'], weights="DEFAULT")
+    model.load_state_dict(model_checkpoint['model_state_dict'])
+    class_to_idx = model_checkpoint['class_to_idx']
+    return model, class_to_idx
+
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    with Image.open(image_path) as image:
+        # Resize the image where shortest side is 256 pixels
+        width, height = image.size
+        aspect_ratio = width / height
+        if width < height:
+            new_width = 256
+            new_height = int(new_width / aspect_ratio)
+        elif width > height:
+            new_height = 256
+            new_width = int(new_height * aspect_ratio)
+        else: # when both sides are equal
+            new_width = 256
+            new_height = 256
+
+        image = image.resize((new_width, new_height))
+
+        # Crop the center 224x224 portion
+        left_margin = (new_width - 224) / 2
+        bottom_margin = (new_height - 224) / 2
+        right_margin = left_margin + 224
+        top_margin = bottom_margin + 224
+
+        image = image.crop((left_margin, bottom_margin, right_margin, top_margin))
+
+        # Normalize
+        np_image = np.array(image) / 255 # Scale pixel values
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        np_image = (np_image - mean) / std # Normalize
+
+        # Reorder dimensions
+        np_image = np_image.transpose((2, 0, 1))
+
+    return np_image
+
+def predict(image_path, model, class_to_idx, use_gpu, topk):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    device = torch.device("cuda" if use_gpu else "cpu")
+    model = model.eval()
+    img = process_image(image_path)
+    probabilities = model(torch.from_numpy(img).unsqueeze(0).float().to(device))
+    probs, top_class = probabilities.topk(topk, dim=1)
+    inv_dict = dict(map(reversed, class_to_idx.items()))
+    classes = [inv_dict[i.item()] for i in top_class.flatten()]
+    return torch.exp(probs), classes
+def main():
+    in_arg = get_input_args()
+    path_to_image = in_arg.path_to_image
+    checkpoint = in_arg.checkpoint
+    top_k = in_arg.top_k
+    categories = in_arg.category_names
+    use_gpu = in_arg.gpu
+
+    model, class_to_idx = load_model(checkpoint)
+    probs, classes = predict(path_to_image, model, class_to_idx, use_gpu, top_k)
+
+    # convert tensor to numpy array and flatten it
+    probs_np = probs.detach().cpu().flatten().numpy()
+    # create pairs of categories and probabilities
+    pairs = list(zip(categories, probs_np))
+    # sort pairs in descending order of probabilities
+    pairs.sort(key=lambda x: x[1])
+    # separate sorted categories and probabilities
+    categories_sorted, probs_sorted = zip(*pairs)
+    return categories_sorted, probs_sorted
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -8,7 +8,12 @@
 #   * Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
 #   * Use GPU for training: python train.py data_dir --gpu
 import argparse
+import torch
+from torch import nn
+from torch import optim
 from torchvision import datasets, transforms, models
+from collections import OrderedDict
+
 
 def get_input_args():
     parser = argparse.ArgumentParser()
@@ -16,8 +21,8 @@ def get_input_args():
                         help='path to the image folder')
     parser.add_argument('save_dir', type=str,
                         help='path to where the model shall be saved', default=".")
-    parser.add_argument('--arch', type=str, default='vgg', choices=['vgg', 'alexnet', 'resnet'],
-                        help='CNN Model Architecture (resnet, alexnet, or vgg)')
+    parser.add_argument('--arch', type=str, default='vgg11',
+                        help='CNN Model Architecture (pick any from https://pytorch.org/vision/stable/models.html#classification)')
     parser.add_argument('--epochs', type=int, default=20, action='store_true',
                         help='Training epochs')
     parser.add_argument('--learning_rate', type=float, default=0.01,
@@ -29,23 +34,94 @@ def get_input_args():
     return parser.parse_args()
 
 
-def select_model(model):
-    return models.get_model(model, weights="DEFAULT")
+def select_model(model, hidden_units):
+    selected_model = models.get_model(model, weights="DEFAULT")
+    classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(hidden_units, 102)),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
+
+    model.classifier = classifier
+
+    return selected_model
 
 
-def train_model(model, dir, epochs, learning_rate, hidden_units, useGpu):
-    train_image_dataset = datasets.ImageFolder(dir, transform=transforms.Compose([
+def train_model(model, dir, epochs, learning_rate, use_gpu):
+    train_dataloaders = datasets.ImageFolder(dir, transform=transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]))
-    pass
+    test_dataloaders = datasets.ImageFolder(dir, transform=transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])]))
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+    device = torch.device("cuda" if use_gpu else "cpu")
+    model.to(device)
+    steps = 0
+    running_loss = 0
+    print_every = 10
+    for epoch in range(epochs):
+        for inputs, labels in train_dataloaders:
+            steps += 1
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in test_dataloaders:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+
+                        test_loss += batch_loss.item()
+
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                print(f"Epoch {epoch + 1}/{epochs}.. "
+                      f"Train loss: {running_loss / print_every:.3f}.. "
+                      f"Test loss: {test_loss / len(test_dataloaders):.3f}.. "
+                      f"Test accuracy: {accuracy / len(test_dataloaders):.3f}")
+                running_loss = 0
+                model.train()
+    return model, train_dataloaders.class_to_idx, optimizer
+
+
+def save_model(model, class_to_idx, epochs, optimizer, arch):
+    torch.save({
+        'class_to_idx': class_to_idx,
+        'model_state_dict': model.state_dict(),
+        # 'optimizer_state_dict': optimizer.state_dict(),
+        # 'epochs': epochs,
+        'arch': arch
+    }, 'model.pth')
 
 
 def main():
     in_arg = get_input_args()
-    dir = in_arg.dir
+    data_dir = in_arg.dir
     save_dir = in_arg.save_dir
     epochs = in_arg.epochs
     learning_rate = in_arg.learning_rate
@@ -53,8 +129,9 @@ def main():
     arch = in_arg.arch
     useGpu = in_arg.gpu
 
-    model = select_model(arch)
-    train_model(model, dir, epochs, learning_rate, hidden_units, useGpu)
+    model = select_model(arch, hidden_units)
+    model, class_to_idx, optimizer = train_model(model, data_dir, epochs, learning_rate, useGpu)
+    save_model(model, class_to_idx, epochs, optimizer, arch)
 
 
 # Call to main function to run the program
